20fa5d230d7ba21c0499cc57bc8219c0a6133bea
A Kline <angela.kline@me.com>
2021-10-03 19:35:35 -0700

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,15 @@ package main
 import "fmt"
 
 func main() {
-        fmt.Println("Hello, World!")
-        fmt.Println("Printing", string("Go"), int(42), bool(true))
+        var s string = "Printing"
+        var t string = "TODO!"
+        var g string = "Go"
+        var i int = 42
+        var b bool = true
+
+        fmt.Println("Hello, World!\r\n")
+        fmt.Println(s, t)
+        fmt.Println(s, g)
+        fmt.Println(s, i)
+        fmt.Println(s, b)
 }
